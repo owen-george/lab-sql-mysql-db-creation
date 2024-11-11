57a2ae5e5ec49af96ac59a056adcbae9dd615880
Owen George <olgeorge@hotmail.co.uk>
2024-11-11 12:16:18 +0000

    Updated create.sql

--- create.sql
+++ create.sql
@@ -4,23 +4,40 @@ USE lab_mysql;
 
 DROP TABLE IF EXISTS cars;
 
-CREATE TABLE cars (id bigint, vin char(255), manufacturer char(255), model char(255), year year, color char(255),
-PRIMARY KEY (id));
+CREATE TABLE cars (id int not null auto_increment primary key,
+	vin char(255) not null,
+	manufacturer char(255) not null,
+	model char(255) not null, 
+	year year not null,
+	color char(255)
+);
 
 DROP TABLE IF EXISTS salespersons;
 
-CREATE TABLE salespersons (id bigint, staff_id bigint, name char(255), store char(255),
-primary key (id));
+CREATE TABLE salespersons (id int not null auto_increment primary key,
+	staff_id bigint not null,
+    name char(255) not null,
+    store char(255) not null
+);
 
 DROP TABLE IF EXISTS customers;
 
-CREATE TABLE customers (id bigint, cust_id bigint, cust_name char(255), 
-cust_phone char(255), cust_email char(255), cust_address char(255),
-cust_city char(255), cust_state char(255), cust_country char(255), cust_zipcode char(255),
-primary key (id));
+CREATE TABLE customers (id int not null auto_increment primary key,
+	cust_id int not null, cust_name char(255) not null, 
+	cust_phone char(255), cust_email char(255), cust_address char(255),
+	cust_city char(255), cust_state char(255), cust_country char(255), cust_zipcode char(255)
+);
 
 DROP TABLE IF EXISTS invoices;
 
-CREATE TABLE invoices (id bigint, invoice_number bigint, date date, car bigint, 
-customer bigint, salesperson bigint,
-PRIMARY KEY (id));
+CREATE TABLE invoices (
+	id int not null auto_increment primary key,
+	invoice_number bigint not null,
+	date date not null,
+    car bigint not null,
+	customer bigint not null,
+	salesperson bigint not null,
+    CONSTRAINT fk_car FOREIGN KEY (car) REFERENCES cars(id),
+    CONSTRAINT fk_customer FOREIGN KEY (customer) REFERENCES customers(id),
+    CONSTRAINT fk_salesperson FOREIGN KEY (salesperson) REFERENCES salespersons(id)
+);
